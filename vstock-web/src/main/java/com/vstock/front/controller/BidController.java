157bab7f0762b71b07e17d0ed119df7b7bb72056
ligang <1421064158@qq.com>
2016-12-13 01:13:49 +0800

    个人页面修改,增加更多记录页面

--- vstock-web/src/main/java/com/vstock/front/controller/BidController.java
+++ vstock-web/src/main/java/com/vstock/front/controller/BidController.java
@@ -1,20 +1,8 @@
 package com.vstock.front.controller;
 
-import com.vstock.db.entity.Bid;
-import com.vstock.db.entity.Trade;
-import com.vstock.db.entity.User;
 import com.vstock.ext.base.BaseController;
-import com.vstock.ext.util.Page;
-import com.vstock.front.service.BidService;
-import com.vstock.front.service.TradeService;
-import com.vstock.front.service.UserService;
-import org.apache.log4j.Logger;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.util.List;
 
 /**
  * Created by administor on 2016/12/6.
