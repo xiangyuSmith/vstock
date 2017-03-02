b280491bf9886abbbc9eb304784341a58a03f2d1
ligang <1421064158@qq.com>
2017-03-02 20:29:28 +0800

    涨幅表的定时任务，修改历史存历史表、涨幅表就存当天的值

--- vstock-web/src/main/java/com/vstock/front/service/ResultDataService.java
+++ vstock-web/src/main/java/com/vstock/front/service/ResultDataService.java
@@ -133,46 +133,50 @@ public class ResultDataService {
      * @return
      */
     public int insertRose(){
-        Basicinformation record = new Basicinformation();
-        DecimalFormat df = new DecimalFormat("######0.000000");
         int i = 0;
-        List<Basicinformation> basicinformationList = basicinformationDao.findAll(record);
-        for (Basicinformation basicinformation : basicinformationList){
-            ResultData resultData = new ResultData();
-            resultData.setBasiciformationId(basicinformation.getId());
-            resultData.setCreateTime(DateUtils.dateToString(DateUtils.wantToLose(new Date(),1),"yyyy-MM-dd"));
-            List<ResultData> resultDatas= this.findResultDataAll(resultData);
-            Map<String,Double> resultDataList = this.sizePrice(resultDatas);
-            resultData.setCreateTime(DateUtils.dateToString(DateUtils.wantToLose(new Date(),2),"yyyy-MM-dd"));
-            List<ResultData> resultDataes = this.findResultDataAll(resultData);
-            Map<String,Double> resultDataLists = this.sizePrice(resultDataes);
-            for (String size : Basicinformation.sizes){
-                BasicinformationRose basicinformationRose = new BasicinformationRose();
-                double totalAmount = 0;
-                totalAmount = this.price(size,resultDataList);
-                basicinformationRose.setCurrent_market_value(new BigDecimal(df.format(totalAmount)));
-                totalAmount = 0;
-                totalAmount = this.price(size,resultDataLists);
-                String nowAmount = basicinformationRose.getCurrent_market_value().subtract(new BigDecimal(totalAmount)).toString();
-                if (nowAmount.contains("-")){
-                    basicinformationRose.setType(0);
-                    logger.info("相减的涨幅度的值"+nowAmount);
-                    basicinformationRose.setChange_range(new BigDecimal(nowAmount.substring(1,nowAmount.length())));
-                }else {
-                    basicinformationRose.setType(1);
-                    basicinformationRose.setChange_range(new BigDecimal(nowAmount));
+        i = basicinformationRoseDao.insertLog();
+        basicinformationRoseDao.del();
+        if (i > 0) {
+            Basicinformation record = new Basicinformation();
+            DecimalFormat df = new DecimalFormat("######0.000000");
+            List<Basicinformation> basicinformationList = basicinformationDao.findAll(record);
+            for (Basicinformation basicinformation : basicinformationList) {
+                ResultData resultData = new ResultData();
+                resultData.setBasiciformationId(basicinformation.getId());
+                resultData.setCreateTime(DateUtils.dateToString(DateUtils.wantToLose(new Date(), 1), "yyyy-MM-dd"));
+                List<ResultData> resultDatas = this.findResultDataAll(resultData);
+                Map<String, Double> resultDataList = this.sizePrice(resultDatas);
+                resultData.setCreateTime(DateUtils.dateToString(DateUtils.wantToLose(new Date(), 2), "yyyy-MM-dd"));
+                List<ResultData> resultDataes = this.findResultDataAll(resultData);
+                Map<String, Double> resultDataLists = this.sizePrice(resultDataes);
+                for (String size : Basicinformation.sizes) {
+                    BasicinformationRose basicinformationRose = new BasicinformationRose();
+                    double totalAmount = 0;
+                    totalAmount = this.price(size, resultDataList);
+                    basicinformationRose.setCurrent_market_value(new BigDecimal(df.format(totalAmount)));
+                    totalAmount = 0;
+                    totalAmount = this.price(size, resultDataLists);
+                    String nowAmount = basicinformationRose.getCurrent_market_value().subtract(new BigDecimal(totalAmount)).toString();
+                    if (nowAmount.contains("-")) {
+                        basicinformationRose.setType(0);
+                        logger.info("相减的涨幅度的值" + nowAmount);
+                        basicinformationRose.setChange_range(new BigDecimal(nowAmount.substring(1, nowAmount.length())));
+                    } else {
+                        basicinformationRose.setType(1);
+                        basicinformationRose.setChange_range(new BigDecimal(nowAmount));
+                    }
+                    if (totalAmount == 0) {
+                        basicinformationRose.setPercentage_change(new BigDecimal(0));
+                    } else {
+                        String percentage_change = df.format(basicinformationRose.getChange_range().doubleValue() / totalAmount);
+                        basicinformationRose.setPercentage_change(new BigDecimal(percentage_change));
+                    }
+                    basicinformationRose.setBrand(basicinformation.getBrand());
+                    basicinformationRose.setBasicinformation_id(Integer.parseInt(basicinformation.getId()));
+                    basicinformationRose.setBasicinformation_size(size);
+                    basicinformationRose.setCreate_date(DateUtils.dateToString(new Date(), "yyyy-MM-dd"));
+                    i = basicinformationRoseDao.insert(basicinformationRose);
                 }
-                if (totalAmount == 0){
-                    basicinformationRose.setPercentage_change(new BigDecimal(0));
-                }else {
-                    String percentage_change = df.format(basicinformationRose.getChange_range().doubleValue()/totalAmount);
-                    basicinformationRose.setPercentage_change(new BigDecimal(percentage_change));
-                }
-                basicinformationRose.setBrand(basicinformation.getBrand());
-                basicinformationRose.setBasicinformation_id(Integer.parseInt(basicinformation.getId()));
-                basicinformationRose.setBasicinformation_size(size);
-                basicinformationRose.setCreate_date(DateUtils.dateToString(new Date(),"yyyy-MM-dd"));
-                i = basicinformationRoseDao.insert(basicinformationRose);
             }
         }
         return i;
