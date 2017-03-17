59c8129d00851345b53582294324f51169afb461
xiangyu <562603138@qq.com>
2017-03-17 18:54:19 +0800

    修改 动态获取支付宝回调地址

--- vstock-server/src/main/java/com/vstock/server/alipay/config/AlipayConfig.java
+++ vstock-server/src/main/java/com/vstock/server/alipay/config/AlipayConfig.java
@@ -12,12 +12,15 @@ package com.vstock.server.alipay.config;
  */
 
 public class AlipayConfig {
-	
+
 //↓↓↓↓↓↓↓↓↓↓请在这里配置您的基本信息↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓
 
 	// 合作身份者ID，签约账号，以2088开头由16位纯数字组成的字符串，查看地址：https://b.alipay.com/order/pidAndKey.htm
 	public static String partner = "2088711791661586";
-	
+
+	//域名
+	public static String now_url = "http://116.228.89.158:1236";
+
 	// 收款支付宝账号，以2088开头由16位纯数字组成的字符串，一般情况下收款账号就是签约账号
 	public static String seller_id = partner;
 
@@ -36,32 +39,32 @@ public class AlipayConfig {
 			"3dizS1GQGi97CA2WHMdvq5e1mZz7PgPW9gXWShS5CjJCYPXEy6+HSQJBAJwdyO64\n" +
 			"ZD1u+MrCdJJjZ3O64BuEz4FiIThMCslVp0Pu4/L5WADFY+/pjScw+8wKFwo3Fobq\n" +
 			"DHWGN/poz4D9GPU=";
-	
+
 	// 支付宝的公钥,查看地址：https://b.alipay.com/order/pidAndKey.htm
 	public static String alipay_public_key  = "MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCnxj/9qwVfgoUh/y2W89L6BkRAFljhNhgPdyPuBV64bfQNN1PjbCzkIM6qRdKBoLPXmKKMiFYnkd6rAoprih3/PrQEB/VsW8OoM8fxn67UDYuyBTqA23MML9q1+ilIZwBC2AQ2UBVOrFXfFl75p6/B5KsiNG9zpgmLCUYuLkxpLQIDAQAB";
 
 	// 服务器异步通知页面路径  需http://格式的完整路径，不能加?id=123这类自定义参数，必须外网可以正常访问
-	public static String notify_url = "http://www.v-stock.com/bid/returnBid";
+	public static String notify_url = now_url + "/bid/returnBid";
 
-	public static String notify_trade_url = "http://www.v-stock.com/trade/returnTradePay";
+	public static String notify_trade_url = now_url + "/trade/returnTradePay";
 
 	// 页面跳转同步通知页面路径 需http://格式的完整路径，不能加?id=123这类自定义参数，必须外网可以正常访问
-	public static String return_url = "http://www.v-stock.com/bid/createPay";
+	public static String return_url = now_url + "/bid/createPay";
 
-	public static String return_trade_url = "http://www.v-stock.com/trade/createTradePay";
+	public static String return_trade_url = now_url + "/trade/createTradePay";
 
 	// 签名方式
 	public static String sign_type = "RSA";
-	
+
 	// 调试用，创建TXT日志文件夹路径，见AlipayCore.java类中的logResult(String sWord)打印方法。
 	public static String log_path = "C:\\";
-		
+
 	// 字符编码格式 目前支持 gbk 或 utf-8
 	public static String input_charset = "utf-8";
-		
+
 	// 支付类型 ，无需修改
 	public static String payment_type = "1";
-		
+
 	// 调用的接口名，无需修改
 	public static String service = "create_direct_pay_by_user";
 
@@ -75,16 +78,16 @@ public class AlipayConfig {
 
 
 //↑↑↑↑↑↑↑↑↑↑请在这里配置您的基本信息↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑
-	
+
 //↓↓↓↓↓↓↓↓↓↓ 请在这里配置防钓鱼信息，如果没开通防钓鱼功能，为空即可 ↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓
-	
+
 	// 防钓鱼时间戳  若要使用请调用类文件submit中的query_timestamp函数
 	public static String anti_phishing_key = "";
-	
+
 	// 客户端的IP地址 非局域网的外网IP地址，如：221.0.0.1
 	public static String exter_invoke_ip = "";
-		
+
 //↑↑↑↑↑↑↑↑↑↑请在这里配置防钓鱼信息，如果没开通防钓鱼功能，为空即可 ↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑
-	
+
 }
 
