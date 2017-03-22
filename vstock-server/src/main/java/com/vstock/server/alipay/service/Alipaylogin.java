046b4adadc8c1fa67e82320ad4539a119156c369
ligang <1421064158@qq.com>
2017-03-22 18:22:41 +0800

    修改支付宝登录方法和快递单号查询

---
+++ vstock-server/src/main/java/com/vstock/server/alipay/service/Alipaylogin.java
@@ -0,0 +1,26 @@
+package com.vstock.server.alipay.service;
+
+import com.vstock.server.alipay.config.AlipayConfig;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Alipaylogin {
+
+    public static Map<String, String> alipayLogin(){
+        Map<String, String> param = new HashMap<String, String>();
+        String service = AlipayConfig.loginservice;
+        String partner = AlipayConfig.partner;
+        String _input_charset = AlipayConfig.input_charset;
+        String sign_type = AlipayConfig.sign_type;
+        String return_url = AlipayConfig.login_return_url;
+        String target_service = AlipayConfig.target_service;
+        param.put("service",service);
+        param.put("partner",partner);
+        param.put("_input_charset",_input_charset);
+        param.put("sign_type",sign_type);
+        param.put("return_url",return_url);
+        param.put("target_service",target_service);
+        return param;
+    }
+}
