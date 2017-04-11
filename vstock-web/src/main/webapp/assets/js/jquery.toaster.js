52360c6f7a7a43e4e40dc61132822ff9b3255757
zengqiang <sunnys>
2017-04-11 18:37:54 +0800

    解决提示框无数次显示的问题，并且去掉关闭

--- vstock-web/src/main/webapp/assets/js/jquery.toaster.js
+++ vstock-web/src/main/webapp/assets/js/jquery.toaster.js
@@ -112,10 +112,10 @@ var $toast_tatol = [];
 		{
 			'template' :
 			'<div class="alert alert-%priority% alert-dismissible" role="alert">' +
-				'<button type="button" class="close" data-dismiss="alert">' +
+				/*'<button type="button" class="close" data-dismiss="alert">' +
 					'<span aria-hidden="true">&times;</span>' +
 					'<span class="sr-only">Close</span>' +
-				'</button>' +
+				'</button>' +*/
 				'<span class="title"></span>: <span class="message"></span>' +
 			'</div>',
 
